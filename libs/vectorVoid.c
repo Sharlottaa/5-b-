08badd31a7de313f8711273b6db295c3c0a68496
Sharlottaa <dasha.sidorenko1111@mail.ru>
2022-02-03 15:02:24 +0300

    memory usage of vectorVoid

--- libs/vectorVoid.c
+++ libs/vectorVoid.c
@@ -1,5 +1,47 @@
 //
 // Created by ну я on 03.02.2022.
 //
-
+#include <stdio.h>
+#include <malloc.h>
 #include "vectorVoid.h"
+
+//возвращает структуру-дескриптор вектор из n значений.
+vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
+    int *a = (int *) malloc(baseTypeSize * n);
+    if (NULL == a) {
+        fprintf(stderr, "bad alloc ");
+        exit(1);
+    }
+    return (vectorVoid) {a, 0, n, baseTypeSize};
+}
+
+//изменяет количество памяти, выделенное под хранение элементов вектора
+void reserveV(vectorVoid *v, size_t newCapacity) {
+    if (newCapacity == 0) {
+        v->data = NULL;
+    } else if (newCapacity < v->size) {
+        v->size = newCapacity;
+    } else {
+        int *a = realloc(v->data, v->baseTypeSize * newCapacity);
+        if (NULL == a) {
+            fprintf(stderr, "bad alloc ");
+            exit(1);
+        }
+        v->capacity = newCapacity;
+    }
+}
+
+//удаляет элементы из контейнера, но не освобождает выделенную память.
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+// освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFitV(vectorVoid *v) {
+    reserveV(v, v->size);
+}
+
+//освобождает память, выделенную вектору
+void deleteVectorV(vectorVoid *v) {
+    free(v->size);
+}
--- libs/vectorVoid.h
+++ libs/vectorVoid.h
@@ -14,6 +14,20 @@ typedef struct vectorVoid {
     size_t baseTypeSize; // размер базового типа:
 } vectorVoid;
 
+//возвращает структуру-дескриптор вектор из n значений.
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+//изменяет количество памяти, выделенное под хранение элементов вектора
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+//удаляет элементы из контейнера, но не освобождает выделенную память.
+void clearV(vectorVoid *v);
+
+// освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFitV(vectorVoid *v);
+
+//освобождает память, выделенную вектору
+void deleteVectorV(vectorVoid *v);
 
 
 #endif //LABA5BVECTORS_VECTORVOID_H
